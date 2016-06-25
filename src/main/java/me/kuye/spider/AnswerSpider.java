9760cd300fbe8f01d241f75687b23e84031ce1b5
xianyijun <xianyijun@creatshare.com>
2016-06-25 21:33:17 +0800

    finish upvoteUser spider demo

--- src/main/java/me/kuye/spider/AnswerSpider.java
+++ src/main/java/me/kuye/spider/AnswerSpider.java
@@ -1,6 +1,8 @@
 package me.kuye.spider;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -13,17 +15,19 @@ import com.alibaba.fastjson.JSON;
 
 import me.kuye.spider.downloader.HttpDownloader;
 import me.kuye.spider.vo.UpVoteResult;
+import me.kuye.spider.vo.UpVoteUser;
 
 public class AnswerSpider {
 	public static void main(String[] args) {
 		/// answer/38441951/voters_profile?total=175&offset=30
-		String upvoteUrl = "/answer/38441951/voters_profile?total=175&offset=10";
+		String upvoteUrl = "/answer/38441951/voters_profile?total=175&offset=0";
 		HttpDownloader downloader = new HttpDownloader();
 		CloseableHttpClient client = downloader.getHttpClient(null);
 		HttpGet request = null;
 		CloseableHttpResponse response = null;
 		UpVoteResult upVoteResult = null;
 		String result = "";
+		List<UpVoteUser> userList = new LinkedList<>();
 		try {
 			while (upvoteUrl != null && !upvoteUrl.equals("")) {
 				request = new HttpGet("https://www.zhihu.com" + upvoteUrl);
@@ -33,7 +37,15 @@ public class AnswerSpider {
 				String[] payload = upVoteResult.getPayload();
 				for (int i = 0; i < payload.length; i++) {
 					Document doc = Jsoup.parse(payload[i]);
-					System.out.println(doc.select(".zg-link").text());
+					UpVoteUser upVoteUser = new UpVoteUser();
+					upVoteUser.setName(doc.select(".zg-link").attr("title"));
+					upVoteUser.setAvatar(doc.select("img.zm-item-img-avatar").attr("src"));
+					upVoteUser.setBio(doc.select(".bio").text());
+					upVoteUser.setAgree(doc.select(".status").first().child(0).text());
+					upVoteUser.setThanks(doc.select(".status").first().child(1).text());
+					upVoteUser.setAnswers(doc.select(".status").first().child(3).text());
+					upVoteUser.setAsks(doc.select(".status").first().child(2).text());
+					userList.add(upVoteUser);
 				}
 				upvoteUrl = upVoteResult.getPaging().getNext();
 			}
---
+++ src/main/java/me/kuye/spider/vo/UpVoteUser.java
@@ -0,0 +1,135 @@
+package me.kuye.spider.vo;
+
+public class UpVoteUser {
+	private String avatar;
+	private String name;
+	private String bio;
+	private String agree;
+	private String thanks;
+	private String asks;
+	private String answers;
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public String getAgree() {
+		return agree;
+	}
+
+	public void setAgree(String agree) {
+		this.agree = agree;
+	}
+
+	public String getThanks() {
+		return thanks;
+	}
+
+	public void setThanks(String thanks) {
+		this.thanks = thanks;
+	}
+
+	public String getAsks() {
+		return asks;
+	}
+
+	public void setAsks(String asks) {
+		this.asks = asks;
+	}
+
+	public String getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(String answers) {
+		this.answers = answers;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((agree == null) ? 0 : agree.hashCode());
+		result = prime * result + ((answers == null) ? 0 : answers.hashCode());
+		result = prime * result + ((asks == null) ? 0 : asks.hashCode());
+		result = prime * result + ((avatar == null) ? 0 : avatar.hashCode());
+		result = prime * result + ((bio == null) ? 0 : bio.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((thanks == null) ? 0 : thanks.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UpVoteUser other = (UpVoteUser) obj;
+		if (agree == null) {
+			if (other.agree != null)
+				return false;
+		} else if (!agree.equals(other.agree))
+			return false;
+		if (answers == null) {
+			if (other.answers != null)
+				return false;
+		} else if (!answers.equals(other.answers))
+			return false;
+		if (asks == null) {
+			if (other.asks != null)
+				return false;
+		} else if (!asks.equals(other.asks))
+			return false;
+		if (avatar == null) {
+			if (other.avatar != null)
+				return false;
+		} else if (!avatar.equals(other.avatar))
+			return false;
+		if (bio == null) {
+			if (other.bio != null)
+				return false;
+		} else if (!bio.equals(other.bio))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (thanks == null) {
+			if (other.thanks != null)
+				return false;
+		} else if (!thanks.equals(other.thanks))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UpVoteUser [avatar=" + avatar + ", name=" + name + ", bio=" + bio + ", agree=" + agree + ", thanks="
+				+ thanks + ", asks=" + asks + ", answers=" + answers + "]";
+	}
+
+}
