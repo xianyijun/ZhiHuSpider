264cac91feef279cd70995cadf382ef7e21782c8
xianyijun <xianyijun@creatshare.com>
2016-06-25 19:57:46 +0800

    fix proccesAnswerDetail

--- src/main/java/me/kuye/spider/QuestionSpider.java
+++ src/main/java/me/kuye/spider/QuestionSpider.java
@@ -33,7 +33,8 @@ public class QuestionSpider {
 
 			int answerNum = Integer.parseInt(doc.select("#zh-question-answer-num").attr("data-num"));
 			question.setAnswerNum(answerNum);
-
+			
+			// 只有登录才存在
 			int visitTimes = Integer.parseInt(doc.select("div.zg-gray-normal strong").eq(1).text());
 			question.setVisitTimes(visitTimes);
 
@@ -53,32 +54,35 @@ public class QuestionSpider {
 				Element element = (Element) elementList[i];
 				Answer answer = new Answer(element.attr("href"), element.baseUri() + element.attr("href"));
 				answer.setQuestion(question);
-				Document answerDoc = Jsoup.connect(answer.getAbsUrl()).get();
-				System.out.println(answer.getAbsUrl());
-				answer.setContent(
-						answerDoc.select("div[data-entry-url=" + answer.getRelativeUrl() + "]  .zm-editable-content")
-								.html().replaceAll("<br>", ""));
-				answer.setUpvote(Long.parseLong(answerDoc.select(".zm-votebar span.count").first().text()));
-				/*
-				 * 不可以直接用a.author-link来获取，如果是知乎用户的话，不存在该标签
-				 * #zh-question-answer-wrap > div > div.answer-head >
-				 * div.zm-item-answer-author-info > a.author-link 普通用户 2
-				 * #zh-question-answer-wrap > div > div.answer-head >
-				 * div.zm-item-answer-author-info > span 知乎用户 2
-				 * #zh-question-answer-wrap > div > div.answer-head >
-				 * div.zm-item-answer-author-info > span 匿名用户 1
-				 * 我们可以先通过获取zm-item-answer-author-info来获取用户名，因为是否为知乎用户的话
-				 */
-				Element authorInfo = answerDoc.select(".zm-item-answer-author-info").first();
-				String author = authorInfo.select(".name").size() == 0 ? authorInfo.select("a.author-link").text()
-						: authorInfo.select(".name").text();
-				answer.setAuthor(author);
-
-				System.out.println(answerDoc.select(".zm-item-answer").attr("data-aid"));
+				processAnswerDetail(answer);
+				System.out.println(answer);
+				System.out.println("///=======================================================");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			response.close();
 		}
+	}
+
+	private static void processAnswerDetail(Answer answer) throws IOException {
+		Document answerDoc = Jsoup.connect(answer.getAbsUrl()).get();
+		answer.setContent(answerDoc.select("div[data-entry-url=" + answer.getRelativeUrl() + "]  .zm-editable-content")
+				.html().replaceAll("<br>", ""));
+		answer.setUpvote(Long.parseLong(answerDoc.select(".zm-votebar span.count").first().text()));
+		/*
+		 * 不可以直接用a.author-link来获取，如果是知乎用户的话，不存在该标签 #zh-question-answer-wrap >
+		 * div > div.answer-head > div.zm-item-answer-author-info >
+		 * a.author-link 普通用户 2 #zh-question-answer-wrap > div > div.answer-head
+		 * > div.zm-item-answer-author-info > span 知乎用户 2
+		 * #zh-question-answer-wrap > div > div.answer-head >
+		 * div.zm-item-answer-author-info > span 匿名用户 1
+		 * 我们可以先通过获取zm-item-answer-author-info来获取用户名，因为是否为知乎用户的话
+		 */
+		Element authorInfo = answerDoc.select(".zm-item-answer-author-info").first();
+		String author = authorInfo.select(".name").size() == 0 ? authorInfo.select("a.author-link").text()
+				: authorInfo.select(".name").text();
+		answer.setAuthor(author);
+
+//		System.out.println(answerDoc.select(".zm-item-answer").attr("data-aid"));
 	}
 }
--- src/main/java/me/kuye/spider/entity/Answer.java
+++ src/main/java/me/kuye/spider/entity/Answer.java
@@ -9,7 +9,7 @@ public class Answer {
 	private String author;
 	private long upvote;
 	private String content;
-	
+
 	public Answer(String relativeUrl, String absUrl) {
 		this.relativeUrl = relativeUrl;
 		this.absUrl = absUrl;
@@ -76,6 +76,70 @@ public class Answer {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((absUrl == null) ? 0 : absUrl.hashCode());
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((question == null) ? 0 : question.hashCode());
+		result = prime * result + ((relativeUrl == null) ? 0 : relativeUrl.hashCode());
+		result = prime * result + (int) (upvote ^ (upvote >>> 32));
+		result = prime * result + ((upvoteUserList == null) ? 0 : upvoteUserList.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (absUrl == null) {
+			if (other.absUrl != null)
+				return false;
+		} else if (!absUrl.equals(other.absUrl))
+			return false;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (question == null) {
+			if (other.question != null)
+				return false;
+		} else if (!question.equals(other.question))
+			return false;
+		if (relativeUrl == null) {
+			if (other.relativeUrl != null)
+				return false;
+		} else if (!relativeUrl.equals(other.relativeUrl))
+			return false;
+		if (upvote != other.upvote)
+			return false;
+		if (upvoteUserList == null) {
+			if (other.upvoteUserList != null)
+				return false;
+		} else if (!upvoteUserList.equals(other.upvoteUserList))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [absUrl=" + absUrl + ", relativeUrl=" + relativeUrl + ", question=" + question + ", author="
+				+ author + ", upvote=" + upvote + ", content=" + content + ", upvoteUserList=" + upvoteUserList + "]";
 	}
 
 }
